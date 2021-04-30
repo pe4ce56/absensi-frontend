883f5cc7556a0ee49856aad3da4a8387944aed08
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-04-30 22:04:50 +0700

    Perbaikan pada component _auth_partials

--- src/Components/_auth_partials/Footer.jsx
+++ src/Components/_auth_partials/Footer.jsx
@@ -8,24 +8,8 @@ export default function Footer(props) {
                 <div className="row align-items-center justify-content-xl-between">
                     <div className="col-xl-6">
                         <div className="copyright text-center text-xl-left text-muted">
-                            © 2020 <a href="https://www.creative-tim.com" className="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+                            © {new Date().getFullYear()} <a href="https://www.schoolandcollegelistings.com/ID/Malang/193668737354797/SMKN-2-Singosari" className="font-weight-bold ml-1" >SMKN 2 Singosari DEV Team</a>
                         </div>
-                    </div>
-                    <div className="col-xl-6">
-                        <ul className="nav nav-footer justify-content-center justify-content-xl-end">
-                            <li className="nav-item">
-                                <a href="https://www.creative-tim.com" className="nav-link" target="_blank">Creative Tim</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="https://www.creative-tim.com/presentation" className="nav-link" target="_blank">About Us</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="http://blog.creative-tim.com" className="nav-link" target="_blank">Blog</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" className="nav-link" target="_blank">MIT License</a>
-                            </li>
-                        </ul>
                     </div>
                 </div>
             </div>
