1ca2b7d89fc4812c3eb91e6d8a37450e38e008b8
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2021-05-07 15:26:18 +0700

    proses login berhasil

--- src/container/page/Login/Login.jsx
+++ src/container/page/Login/Login.jsx
@@ -1,26 +1,42 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 import axios from 'axios'
+import swal from 'sweetalert2'
+import BlockUi from 'react-block-ui'
+import 'react-block-ui/style.css'
 import { API_URL } from '../../../config/config'
 import { Navbar, Header, Footer } from '../../../component/Auth/index'
 import LoginForm from './LoginForm/LoginForm'
+import ActionType from '../../../redux/reducer/globalActionType'
 
-export default class Login extends Component {
+class Login extends Component {
     constructor(props) {
         super(props)
-
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            redirectToDashboard: false,
+            toggleBlocking: false
         }
     }
 
     componentDidMount() {
+        if (this.props.store?.auth?.isAuthenticated) {
+            return this.props.history.push('/dashboard')
+        }
+
         const body = document.querySelector('body')
         body.classList.add('bg-default')
 
         // this.setState({
         //     page: this.props.match.url.match(/admin/g) ? 'adm' : 'ts' // => TeacherStudent
         // })
+    }
+
+    componentWillUnmount() {
+        const body = document.querySelector('body')
+        body.classList.remove('bg-default')
     }
 
     setLoginState = ({ username, password }) => {
@@ -34,16 +50,31 @@ export default class Login extends Component {
     }
 
     loginSubmit = (e) => {
+        const { from } = this.props.location.state || { from: { pathname: '/' } }
+
+        this.setState(prevState => ({ ...prevState, toggleBlocking: true }))
         axios.post(`${API_URL}/api/login`, {
             username: this.state.username,
             password: this.state.password
         }).then(({ data }) => {
-            // if (data.code === 200) {
-            //     localStorage.setItem('token', data.data.token)
-            //     localStorage.setItem('user', JSON.stringify(data.data.user))
-            // }
-            // console.log(localStorage.getItem('token'), localStorage.removeItem)
-            console.log(data)
+            if (data.code === 200) {
+                localStorage.setItem('token', data.data.token)
+                localStorage.setItem('user', JSON.stringify(data.data.user))
+                swal.fire({
+                    title: "Login berhasil",
+                    text: "Anda akan dialihkan ke Dashboard",
+                    icon: "success",
+                }).then(will => {
+                    if (will) {
+                        this.props.dispatch.updateUserdata({
+                            isAuthenticated: true,
+                            token: data.data.token,
+                            user: data.data.user
+                        })
+                        return window.location = '/dashboard'
+                    }
+                })
+            }
         })
     }
 
@@ -57,9 +88,11 @@ export default class Login extends Component {
                         <div className="row justify-content-center">
                             <div className="col-lg-5 col-md-7">
                                 <div className="card bg-secondary border-0 mb-0">
-                                    <div className="card-body px-lg-5 py-lg-5">
-                                        <LoginForm setLoginState={this.setLoginState} loginSubmit={this.loginSubmit} />
-                                    </div>
+                                    <BlockUi tag="div" blocking={this.state.toggleBlocking}>
+                                        <div className="card-body px-lg-5 py-lg-5">
+                                            <LoginForm setLoginState={this.setLoginState} loginSubmit={this.loginSubmit} />
+                                        </div>
+                                    </BlockUi>
                                 </div>
                             </div>
                         </div>
@@ -70,3 +103,24 @@ export default class Login extends Component {
         )
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        store: state
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        dispatch: {
+            updateUserdata: (props) => {
+                dispatch({
+                    type: ActionType.UPDATE_USERDATA,
+                    ...props
+                })
+            }
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
